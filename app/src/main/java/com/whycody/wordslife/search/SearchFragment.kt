70b35387f54903e5c3a81dfeacc63af6edf28c4e
Oktawian Kausz <36113911+whycody@users.noreply.github.com>
2021-04-11 19:37:32 +0200

    Added SearchContent fragment

--- app/src/main/java/com/whycody/wordslife/search/SearchFragment.kt
+++ app/src/main/java/com/whycody/wordslife/search/SearchFragment.kt
@@ -5,40 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.whycody.wordslife.R
-import com.whycody.wordslife.databinding.FragmentSearchBinding
-import com.whycody.wordslife.search.result.SearchResultFragment
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class SearchFragment : Fragment() {
 
-    private lateinit var layoutView: View
     private var searchWord = ""
     private val searchViewModel: SearchViewModel by sharedViewModel()
 
-    private val mainLyricsSearchResultFragment =
-            SearchResultFragment.newInstance(SearchResultFragment.MAIN_LYRICS)
-    private val similarLyricsSearchResultFragment =
-            SearchResultFragment.newInstance(SearchResultFragment.SIMILAR_LYRICS)
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        val binding: FragmentSearchBinding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_search, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
         searchWord = arguments?.getString(SEARCH_WORD, "")!!
-        checkSavedInstanceState(savedInstanceState)
-        binding.lifecycleOwner = activity
-        binding.viewModel = searchViewModel
-        layoutView = binding.root
-        return layoutView
-    }
-
-    private fun checkSavedInstanceState(savedInstanceState: Bundle?) {
-        if(savedInstanceState == null) {
-            addFragments()
-            searchViewModel.searchWord(searchWord)
-        }
+        if(savedInstanceState == null) searchViewModel.searchWord(searchWord)
+        return view
     }
 
     override fun onDestroy() {
@@ -47,13 +27,6 @@ class SearchFragment : Fragment() {
     }
 
     private fun resetSearchWord() = searchViewModel.searchWord("")
-    
-    private fun addFragments() {
-        val fragmentTransaction = childFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.fragmentsContainer, mainLyricsSearchResultFragment)
-        fragmentTransaction.add(R.id.fragmentsContainer, similarLyricsSearchResultFragment)
-        fragmentTransaction.commit()
-    }
 
     companion object {
         const val SEARCH_WORD = "search_word"
---
+++ app/src/main/java/com/whycody/wordslife/search/content/SearchContentFragment.kt
@@ -0,0 +1,31 @@
+package com.whycody.wordslife.search.content
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.whycody.wordslife.R
+import com.whycody.wordslife.search.result.SearchResultFragment
+
+class SearchContentFragment: Fragment() {
+
+    private val mainLyricsSearchResultFragment =
+        SearchResultFragment.newInstance(SearchResultFragment.MAIN_LYRICS)
+    private val similarLyricsSearchResultFragment =
+        SearchResultFragment.newInstance(SearchResultFragment.SIMILAR_LYRICS)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_search_content, container, false)
+        if(savedInstanceState == null) addFragments()
+        return view
+    }
+
+    private fun addFragments() {
+        val fragmentTransaction = childFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentsContainer, mainLyricsSearchResultFragment)
+        fragmentTransaction.add(R.id.fragmentsContainer, similarLyricsSearchResultFragment)
+        fragmentTransaction.commit()
+    }
+}
