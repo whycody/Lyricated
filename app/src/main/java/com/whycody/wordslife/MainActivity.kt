6d4310acf1203be2cdb1e2c54596b74f7e51a074
Oktawian Kausz <36113911+whycody@users.noreply.github.com>
2021-04-12 16:16:43 +0200

    Added enter and exit animation on changing fragment

--- app/src/main/java/com/whycody/wordslife/MainActivity.kt
+++ app/src/main/java/com/whycody/wordslife/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity(), MainNavigation {
     override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
         val transaction = supportFragmentManager
                 .beginTransaction()
+                .setCustomAnimations(R.anim.enter_anim, R.anim.exit_anim, R.anim.enter_anim, R.anim.exit_anim)
                 .add(R.id.container, fragment)
         if (addToBackstack) transaction.addToBackStack(null)
         transaction.commit()
