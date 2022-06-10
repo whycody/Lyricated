644074adbe412da3e94b98a439e3e7cb32cf438a
whycody <play26400@gmail.com>
2022-06-10 13:14:29 +0200

    Added TTS

--- app/src/main/java/com/whycody/wordslife/library/studymode/translation/StudyModeTranslationFragment.kt
+++ app/src/main/java/com/whycody/wordslife/library/studymode/translation/StudyModeTranslationFragment.kt
@@ -1,12 +1,16 @@
 package com.whycody.wordslife.library.studymode.translation
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import com.whycody.wordslife.R
+import com.whycody.wordslife.data.LyricLanguages
+import com.whycody.wordslife.data.language.LanguageDao
+import com.whycody.wordslife.data.language.LanguageDaoImpl
 import com.whycody.wordslife.data.utilities.TextCopyUtility
 import com.whycody.wordslife.databinding.FragmentStudyModeTranslationBinding
 import com.whycody.wordslife.library.studymode.StudyModeViewModel
@@ -16,23 +20,26 @@ import com.whycody.wordslife.search.lyric.translation.LyricTranslationViewModel
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.*
 
-class StudyModeTranslationFragment : Fragment() {
+class StudyModeTranslationFragment : Fragment(), TextToSpeech.OnInitListener {
 
     private lateinit var typeOfPhrase: String
     private lateinit var binding: FragmentStudyModeTranslationBinding
     private val textCopyUtility: TextCopyUtility by inject()
     private val studyModeViewModel: StudyModeViewModel by sharedViewModel()
     private val lyricTranslationViewModel: LyricTranslationViewModel by viewModel()
+    private val languageDao: LanguageDao by inject()
+    private lateinit var tts: TextToSpeech
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentStudyModeTranslationBinding.inflate(inflater)
-        binding.translationText.setOnLongClickListener { textCopyUtility.copyText(
-            requireParentFragment().requireView(),
-            lyricTranslationViewModel.getTranslationItem().value!!.translatedSentence!!,
-            parentFragment?.view?.findViewById(R.id.revealWordBtn)) }
+        binding.translationText.setOnLongClickListener { copyText() }
         binding.viewAboveTranslation.setOnLongClickListener { false }
+        binding.viewAboveTranslation.setOnClickListener { }
+        tts = TextToSpeech(requireContext(), this)
+        binding.playTTSBtn.setOnClickListener { playTTS() }
         typeOfPhrase = arguments?.getString(LyricTranslationFragment.TYPE_OF_PHRASE,
             LyricTranslationFragment.MAIN_PHRASE)!!
         addHeader()
@@ -40,6 +47,17 @@ class StudyModeTranslationFragment : Fragment() {
         observeExtendedLyricItem()
         observeTranslationItem()
         return binding.root
+    }
+
+    private fun copyText() = textCopyUtility.copyText(requireParentFragment().requireView(),
+        lyricTranslationViewModel.getTranslationItem().value!!.translatedSentence!!,
+        parentFragment?.view?.findViewById(R.id.revealWordBtn))
+
+    private fun playTTS() {
+        binding.playTTSBtn.startAnimation(AnimationUtils.loadAnimation(requireContext(),
+            R.anim.speaker_pulse_anim))
+        tts.speak(lyricTranslationViewModel.getTranslationItem().value!!.translatedSentence,
+            TextToSpeech.QUEUE_FLUSH, null, null)
     }
 
     private fun addHeader() {
@@ -74,6 +92,31 @@ class StudyModeTranslationFragment : Fragment() {
             if(it==null) return@observe
             lyricTranslationViewModel.findTranslation(it, typeOfPhrase)
         }
+
+    override fun onInit(status: Int) { configureTTS() }
+
+    private fun configureTTS() {
+        val languages = LyricLanguages(languageDao.getCurrentMainLanguage().id,
+            languageDao.getCurrentTranslationLanguage().id)
+        val locale = getLocale(getCurrentLang(languages))
+        binding.ttsAvailable = locale != null
+        if(locale != null) tts.language = locale
+    }
+
+    private fun getCurrentLang(languages: LyricLanguages) =
+        if(typeOfPhrase == LyricTranslationFragment.MAIN_PHRASE) languages.mainLangId
+        else languages.translationLangId
+
+    private fun getLocale(lang: String): Locale? {
+        return when(lang) {
+            LanguageDaoImpl.EN -> Locale.US
+            LanguageDaoImpl.DE -> Locale.GERMAN
+            LanguageDaoImpl.FR -> Locale.FRENCH
+            LanguageDaoImpl.IT -> Locale.ITALIAN
+            LanguageDaoImpl.ES -> Locale("spa")
+            else -> null
+        }
+    }
 
     companion object {
         fun newInstance(typeOfPhrase: String): StudyModeTranslationFragment {
--- app/src/main/java/com/whycody/wordslife/search/lyric/translation/LyricTranslationFragment.kt
+++ app/src/main/java/com/whycody/wordslife/search/lyric/translation/LyricTranslationFragment.kt
@@ -1,12 +1,16 @@
 package com.whycody.wordslife.search.lyric.translation
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.whycody.wordslife.R
+import com.whycody.wordslife.data.LyricLanguages
+import com.whycody.wordslife.data.language.LanguageDaoImpl
 import com.whycody.wordslife.data.utilities.TextCopyUtility
 import com.whycody.wordslife.databinding.FragmentLyricTranslationBinding
 import com.whycody.wordslife.search.lyric.LyricViewModel
@@ -14,26 +18,37 @@ import com.whycody.wordslife.search.lyric.header.HeaderFragment
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.*
 
-class LyricTranslationFragment : Fragment() {
+class LyricTranslationFragment : Fragment(), TextToSpeech.OnInitListener {
 
     private lateinit var typeOfPhrase: String
     private val lyricViewModel: LyricViewModel by sharedViewModel()
     private val lyricTranslationViewModel: LyricTranslationViewModel by viewModel()
     private val textCopyUtility: TextCopyUtility by inject()
     private lateinit var binding: FragmentLyricTranslationBinding
+    private lateinit var tts: TextToSpeech
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = FragmentLyricTranslationBinding.inflate(inflater)
         val view = (parentFragment as BottomSheetDialogFragment).dialog?.window?.decorView!!
+        tts = TextToSpeech(requireContext(), this)
         binding.contentText.setOnLongClickListener { textCopyUtility.copyText(view,
             lyricTranslationViewModel.getTranslationItem().value!!.translatedSentence!!) }
+        binding.playTTSBtn.setOnClickListener { playTTS() }
         typeOfPhrase = arguments?.getString(TYPE_OF_PHRASE, MAIN_PHRASE)!!
         if(savedInstanceState == null) addHeader()
         observeTranslationItem(binding)
         observeExtendedLyricItem()
         return binding.root
+    }
+
+    private fun playTTS() {
+        binding.playTTSBtn.startAnimation(AnimationUtils.loadAnimation(requireContext(),
+            R.anim.speaker_pulse_anim))
+        tts.speak(lyricTranslationViewModel.getTranslationItem().value!!.translatedSentence,
+            TextToSpeech.QUEUE_FLUSH, null, null)
     }
 
     private fun addHeader() {
@@ -57,6 +72,30 @@ class LyricTranslationFragment : Fragment() {
             lyricTranslationViewModel.findTranslation(it, typeOfPhrase)
         }
 
+    override fun onInit(status: Int) { configureTTS() }
+
+    private fun configureTTS() {
+        val languages = lyricViewModel.getCurrentExtendedLyricItem().value!!.languages
+        val locale = getLocale(getCurrentLang(languages))
+        binding.ttsAvailable = locale != null
+        if(locale != null) tts.language = locale
+    }
+
+    private fun getCurrentLang(languages: LyricLanguages) =
+        if(typeOfPhrase == MAIN_PHRASE) languages.mainLangId
+        else languages.translationLangId
+
+    private fun getLocale(lang: String): Locale? {
+        return when(lang) {
+            LanguageDaoImpl.EN -> Locale.US
+            LanguageDaoImpl.DE -> Locale.GERMAN
+            LanguageDaoImpl.FR -> Locale.FRENCH
+            LanguageDaoImpl.IT -> Locale.ITALIAN
+            LanguageDaoImpl.ES -> Locale("spa")
+            else -> null
+        }
+    }
+
     companion object {
         const val TYPE_OF_PHRASE = "type of phrase"
         const val MAIN_PHRASE = "main phrase"
@@ -70,5 +109,4 @@ class LyricTranslationFragment : Fragment() {
             return fragment
         }
     }
-
 }
